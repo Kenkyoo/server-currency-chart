0e209736bb7b6af6cee91640a08f00a550f986d1
Kenkyoo <124638519+Kenkyoo@users.noreply.github.com>
2024-11-22 04:28:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 // Middleware
 app.use(cors());
 
-const url_base = 'https://www.frankfurter.app';
+const url_base = 'https://api.frankfurter.app';
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
